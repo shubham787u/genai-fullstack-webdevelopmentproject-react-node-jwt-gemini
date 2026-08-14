8342ed161bcff7c924641cf9de5450003db349dc
shubham787u <shubh.maa2016@gmail.com>
2026-08-14 13:40:00 +0530

    wip

--- src/config/database.js
+++ src/config/database.js
@@ -7,9 +7,8 @@ async function connectToDB() {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("Connected to database");
-    
   } catch (error) {
-    console.log(err);
+    console.log("Disconnect database", err);
   }
 }
 
